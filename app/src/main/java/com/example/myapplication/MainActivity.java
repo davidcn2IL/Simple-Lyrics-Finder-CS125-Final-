a0a1ce0e9ed605f9493e92fe5672068c83f883ea
davidcn2IL <davidcn2@illinois.edu>
2019-12-01 05:58:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                goToSecondActivity();
+
+            }
+
+        });
+
+    }
+
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this, Main2Activity.class);
+
+        startActivity(intent);
+
+    }
+}
